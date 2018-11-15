e751ee09403059c34f828062e55e40d0346f4438
Joseph Jeganathan <josephjeganathan@gmail.com>
2018-11-15 12:19:19 +1100

    GitHub's 3 different flavors of git merges

---
+++ README.md
@@ -0,0 +1,29 @@
+# Git Merge
+
+**Check it yourself**
+
+Check out the repository run `./run.sh` in git bash and follow the instructions.
+
+# Results
+
+## Multiple branches
+
+![3 Branches][3branches]
+
+## Normal merge
+
+![Normal Merge][normal_merge]
+
+### Sqash Merge
+
+![Sqash Merge][sqash_merge]
+
+### Rebase and Merge
+
+![Rebase and Merge][rebase_and_merge]
+
+
+[3branches]: ./images/3branches.png
+[normal_merge]: ./images/normal_merge.png
+[sqash_merge]: ./images/sqash_merge.png
+[rebase_and_merge]: ./images/rebase_and_merge.png
---
+++ run.sh
@@ -0,0 +1,111 @@
+branch_off() {
+    git checkout master
+    git branch feature/E-00$1_feature$1
+    git checkout feature/E-00$1_feature$1
+}
+
+commit_file_line() {
+    echo "Line $2" >> File$1.txt
+    git add .
+    git commit -m "Added line $2 to file $1"
+}
+
+squash_merge() {
+
+    echo "RUNNING SQUASH MERGE"
+
+    squash_merge_branch() {
+        git checkout master
+        git merge --squash feature/E-00$1_feature$1
+        git commit -m "Added feature E-00$1 feature$1"
+    }
+    squash_merge_branch 1
+    squash_merge_branch 2
+    squash_merge_branch 3
+}
+
+rebase_merge() {
+
+    echo "RUNNING SQUASH REBASE MERGE"
+
+    rebase_merge_branch() {
+        git checkout master
+        git rebase feature/E-00$1_feature$1
+        git merge feature/E-00$1_feature$1
+        git commit -m "Added feature E-00$1 feature$1"
+    }
+    
+    rebase_merge_branch 1
+    rebase_merge_branch 2
+    rebase_merge_branch 3
+}
+
+normal_merge(){
+    
+    echo "RUNNING MORMAL MERGE"
+    
+    normal_merge_branch() {
+        git checkout master
+        git merge feature/E-00$1_feature$1  -m "Added feature E-00$1 feature$1"
+    }
+
+    normal_merge_branch 1
+    normal_merge_branch 2
+    normal_merge_branch 3
+}
+
+delete_branches() {
+    git branch -D feature/E-001_feature1
+    git branch -D feature/E-002_feature2
+    git branch -D feature/E-003_feature3
+}
+
+initialise() {
+
+    rm -rf .git
+    rm -rf *.txt
+
+    git init
+    echo "Git Merge" >> README.md
+    git add .
+    git commit -m "Initial commit"
+
+    branch_off 1
+    commit_file_line 1 1
+    commit_file_line 1 2
+    commit_file_line 1 3
+
+    branch_off 2
+    commit_file_line 2 1
+    commit_file_line 2 2
+    commit_file_line 2 3
+
+    branch_off 3
+    commit_file_line 3 1
+    commit_file_line 3 2
+    commit_file_line 3 3
+}
+while true 
+do
+    initialise
+    echo "s - Squash merge"
+    echo "r - Rebase and merge"
+    echo "<any other key> - Default merge"
+    read -n 1 -p "Select merge style: " mergestyle
+    clear
+
+    if [ "$mergestyle" = "s" ]; then
+        squash_merge
+    elif [ "$mergestyle" = "r" ]; then
+        rebase_merge
+    else
+        normal_merge
+    fi
+
+    delete_branches
+
+    echo "Press any key to try again..."
+    read -n 1
+    clear
+ 
+done
